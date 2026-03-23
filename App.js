56247a2a6e245f8a8f70d5b44055b9b5ae825594
ErShubham4u <connect2ershubham@gmail.com>
2026-03-23 20:11:42 +0530

    Class - 01 ( Part - 04 )

--- App.js
+++ App.js
@@ -6,7 +6,47 @@
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 
-const heading = React.createElement("h1", { id: "heading", xyz : "abc" }, "Hello EveryOne !!!");
-console.log(heading)    // object
+// const heading = React.createElement("h1", { id: "heading", xyz : "abc" }, "Hello EveryOne !!!");
+// console.log(heading)    // object
+
+
+/*
+
+<div id="parent">
+  <div id="child">
+    <h1>I'm h1 tag</h1>
+  </div>
+</div>;
+
+*/
+
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", {}, "I'm h1 tag"),
+//   ),
+// );
+// console.log(parent)    // object
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+);
+
+console.log(parent); // object
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
+
+
+
+
