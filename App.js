e3f9734864ee231aa4f62591f9fd19915fc6cc3b
ErShubham4u <connect2ershubham@gmail.com>
2026-03-24 19:26:22 +0530

    Class - 02 ( Part - 04 )

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1", {id: "heading" }, "Hello EveryOne !!!");
-// console.log(heading)
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-
-
 const jsxHeading = <h1 className="heading">Hello EveryOne !!!</h1>;
 console.log(jsxHeading);
+
+/*
+React Component : 
+Class Based Component - OLD
+Functional Component  - NEW
+*/
+
+// React Functional Component
+
+const MainHeading = () => <h1 className="heading">Hello EveryOne !!! - MainHeading</h1>;
+
+const SubHeading = () => {
+  return <h1 className="heading">Hello EveryOne !!! - SubHeading</h1>;
+};
+
+const Title = () => <h1 className="title">Hello EveryOne !!! - Title</h1>;
+
+const HeadingComponent = () => (
+  <div className="container">
+    <Title />
+    <h1 className="heading">Hello EveryOne !!! - Functional Component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
