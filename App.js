104c1af793948716c09f3810778a2e4dfce17d32
ErShubham4u <connect2ershubham@gmail.com>
2026-03-24 19:15:42 +0530

    Class - 02 ( Part - 03 )

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {id: "heading" }, "Hello EveryOne !!!");
-console.log(heading)
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// const heading = React.createElement("h1", {id: "heading" }, "Hello EveryOne !!!");
+// console.log(heading)
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
 
+
+const jsxHeading = <h1 className="heading">Hello EveryOne !!!</h1>;
+console.log(jsxHeading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
