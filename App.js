f6f40c1a9a03418389bad49f29abd7b1262db94d
ErShubham4u <connect2ershubham@gmail.com>
2026-03-25 21:15:21 +0530

    Class - 03 ( Part - 05 )

--- App.js
+++ App.js
@@ -22,9 +22,8 @@ const Header = () => {
   );
 };
 
-
 const RestaurantCard = (props) => {
-  const { resName, cuisine, stars, time } = props;
+  const { resData } = props;
   return (
     <div
       className="res-card"
@@ -37,25 +36,114 @@ const RestaurantCard = (props) => {
         src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2026/1/8/cf2e2a39-5439-4094-8cce-4b38388c438c_33722.JPG"
         alt="res-logo"
       />
-      <h3>{resName}</h3>
-      <h4>{cuisine}</h4>
-      <h4>{stars} stars</h4>
-      <h4>{time}</h4>
+      <h3>{resData.info.name}</h3>
+      <h4>{resData.info.avgRating} stars</h4>
+      <h4>{resData.info.costForTwo}</h4>
+      <h4>{resData.info.sla.deliveryTime} minutes</h4>
     </div>
   );
 };
+
+const resList = [
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "625226",
+      name: "Pizza Hut",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2026/2/12/3325d667-785a-40ac-8a5e-daa076728446_625226.JPG",
+      locality: "Shukrawar Peth",
+      areaName: "Hirabaug",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.2,
+      parentId: "721",
+      avgRatingString: "4.2",
+      totalRatingsString: "2.4K+",
+      sla: {
+        deliveryTime: 28,
+        lastMileTravel: 2.8,
+        serviceability: "SERVICEABLE",
+        slaString: "25-30 mins",
+        lastMileTravelString: "2.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2026-03-15 23:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-Pizza.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Pizza.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "4.8",
+          ratingCount: "1.3K+",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-8d89fc09-ec16-4815-b92e-2985a59d2c5c",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/pune/pizza-hut-shukrawar-peth-hirabaug-rest625226",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+];
 
 const Body = () => {
   return (
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard
-          resName="KFC"
-          cuisine="Burger, Fas Food"
-          stars="4.2"
-          time="25 minutes"
-        />
+        <RestaurantCard resData={resList[0]} />
       </div>
     </div>
   );
