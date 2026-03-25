b7b6b930cbc29857691678b6680f2409240ce5e7
ErShubham4u <connect2ershubham@gmail.com>
2026-03-25 19:40:24 +0530

    Class - 03 ( Part - 01 )

--- App.js
+++ App.js
@@ -1,27 +1,73 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = <h1 className="heading">Hello EveryOne !!!</h1>;
-console.log(jsxHeading);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://images-platform.99static.com/v84irzbNBd5aawXGKXfH4SEjcn0=/0x0:960x960/500x500/top/smart/99designs-contests-attachments/117/117132/attachment_117132760"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-/*
-React Component : 
-Class Based Component - OLD
-Functional Component  - NEW
-*/
 
-// React Functional Component
+const RestaurantCard = () => {
+  return (
+    <div
+      className="res-card"
+      style={{
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2026/1/8/cf2e2a39-5439-4094-8cce-4b38388c438c_33722.JPG"
+        alt="res-logo"
+      />
+      <h3>Pizza Hut</h3>
+      <h4>Pizza</h4>
+      <h4>4.2 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
 
-const Title = () => <h1 className="title">Hello EveryOne !!! - Title</h1>;
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
 
-const HeadingComponent = () => (
-  <div className="container">
-    {Title()}
-    <Title />
-    <Title></Title>
-    <h1 className="heading">Hello EveryOne !!! - Functional Component</h1>
-  </div>
-);
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
