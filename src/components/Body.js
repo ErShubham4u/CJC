211a346d5de97a8ea98e1486af9c5e15238ca9ed
ErShubham4u <connect2ershubham@gmail.com>
2026-03-28 19:04:18 +0530

    Class - 05 ( Part - 02 )

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,20 +2,27 @@ import RestaurantCard from "./RestaurantCard";
 import { resList } from "../../utils/mockData";
 
 const Body = () => {
+
+  let listOfRestaurants = resList;
+
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            console.log("Button Clicked");
+            // Filter logic here
+            listOfRestaurants = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.0
+            );
+            console.log(listOfRestaurants);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => {
+        {listOfRestaurants.map((restaurant) => {
           return (
             <RestaurantCard key={restaurant.info.id} resData={restaurant} />
           );
