0d78e94fafe4549ed68f6bbb823b8a9b429d24c9
ErShubham4u <connect2ershubham@gmail.com>
2026-03-28 19:16:10 +0530

    Class - 05 ( Part - 05 )

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,34 @@
 import RestaurantCard from "./RestaurantCard";
 import { resList } from "../../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  // useEffect(()=>{
+  //   console.log("useEffect called");
+  // },[])
+  // console.log("Body rendered");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING",
+    );
+    const json = await data.json();
+    console.log(json);
+
+    // setListOfRestaurants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+    // Optional Chaining
+
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants,
+    );
+  };
 
   return (
     <div className="body">
