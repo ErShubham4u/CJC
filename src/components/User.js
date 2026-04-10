6efbaaad6542ca1721f1434a2d7e1ac02201ed68
ErShubham4u <connect2ershubham@gmail.com>
2026-04-10 20:43:32 +0530

    Class - 11 ( Part - 01 )

---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = () => {
+    return (
+        <div className="user-card">
+            <h2>Name : Shubham Kumar</h2>
+            <h3>Location : Patna, Bihar</h3>
+            <h4>Contact : @ErShubham4u</h4>
+        </div>
+    );
+};
+
+export default User;
