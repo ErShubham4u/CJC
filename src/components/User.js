212df5f2356365bbe5e6b074990aa6f9437cd8a6
ErShubham4u <connect2ershubham@gmail.com>
2026-04-10 20:53:41 +0530

    Class - 11 ( Part - 07 )

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
+
 const User = ({ name, location, contact }) => {
+  const [count, setCount] = useState(0);
   return (
     <div className="user-card">
+      <h1>Count : {count}</h1>
       <h2>Name : {name}</h2>
       <h3>Location : {location}</h3>
       <h4>Contact : {contact}</h4>
