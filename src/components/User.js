7f3db7676d6d1911e06d29693c3bffe630b88bc2
ErShubham4u <connect2ershubham@gmail.com>
2026-04-10 21:03:41 +0530

    Class - 11 ( Part - 11 )

--- src/components/User.js
+++ src/components/User.js
@@ -5,6 +5,13 @@ const User = ({ name, location, contact }) => {
   return (
     <div className="user-card">
       <h1>Count : {count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Count Increase
+      </button>
       <h2>Name : {name}</h2>
       <h3>Location : {location}</h3>
       <h4>Contact : {contact}</h4>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -13,6 +13,15 @@ class UserClass extends React.Component {
     return (
       <div className="user-card">
         <h1>Count : {count}</h1>
+        <button
+          onClick={() => {
+            // Never Update State Variables Directly
+            // this.state.count = this.state.count + 1; // Wrong Way To Update State Variables
+            this.setState({ count: count + 1 }); // Correct Way To Update State Variables
+          }}
+        >
+          Count Increase
+        </button>
         <h2>Name : {name}</h2>
         <h3>Location : {location}</h3>
         <h4>Contact : {contact}</h4>
