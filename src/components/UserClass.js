4cc1ed7e820e7690e29dc30019758f7f6c43bd49
ErShubham4u <connect2ershubham@gmail.com>
2026-04-10 20:45:03 +0530

    Class - 11 ( Part - 02 )

---
+++ src/components/UserClass.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class UserClass extends React.Component {
+  render() {
+    return (
+      <div className="user-card">
+        <h2>Name : Shubham Kumar</h2>
+        <h3>Location : Patna, Bihar</h3>
+        <h4>Contact : @ErShubham4u</h4>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
