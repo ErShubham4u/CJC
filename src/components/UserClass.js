5a113cc84c8fad4054d4aa9f30eb9ff8e92aa9d0
ErShubham4u <connect2ershubham@gmail.com>
2026-04-10 20:56:16 +0530

    Class - 11 ( Part - 09 )

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,14 +5,16 @@ class UserClass extends React.Component {
     super(props);
     this.state = {
       count: 0,
+      count2: 1,
     };
   }
   render() {
     const { name, location, contact } = this.props;
-    const { count } = this.state;
+    const { count, count2 } = this.state;
     return (
       <div className="user-card">
-         <h1>Count : {count}</h1>
+        <h1>Count : {count}</h1>
+        <h1>Count2 : {count2}</h1>
         <h2>Name : {name}</h2>
         <h3>Location : {location}</h3>
         <h4>Contact : {contact}</h4>
